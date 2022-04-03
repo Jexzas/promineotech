fbace5b4b8027750010386a07cdc1b0e9a10b34e
Jexzas <jdrmail2007@yahoo.com>
2022-04-03 15:25:09 -0500

    desktop

---
+++ Week 2/grading scale.js
@@ -0,0 +1,35 @@
+var grade = prompt("What percent did you get on the test?");
+// grade = parseInt(grade);
+var gscore; 
+switch (true) {
+    case (grade >= 90 && grade <= 100):
+        gscore = 'A';
+        break;
+    case (grade >= 80 && grade < 90):
+        gscore = 'B';
+        break;
+    case (grade >= 70 && grade < 80):
+        gscore = 'C';
+        break;
+    case (grade >= 60 && grade < 70):
+        gscore = 'D';
+        break;
+    case (grade >= 0 && grade <= 59):
+        gscore = 'F';
+        break;
+    default:
+        gscore = `${grade}??? That's not even an option`;
+        break;
+}
+
+document.open();
+document.write(`<h1 id="uhoh">Congratulations! You got a dang ${gscore}!</h1>`);
+if (gscore == 'A') {
+    let h1 = document.getElementById("uhoh");
+    h1.setAttribute("style", "font-size: 10rem; color: red; text-transform: uppercase;");
+    let body = document.body;
+    body.setAttribute("style", "background-color: black;");
+} else {
+    body.setAttribute("style", "background-color: white;");
+}
+document.close();
--- Week 2/week2garbage.js
+++ Week 2/week2garbage.js
@@ -6,10 +6,13 @@ var walmartMilk = prompt("What is the price of milk (gallon) at Wal-Mart");
 var tankSize = prompt("How many gallons of gas does your car use? This is important.");
 tankSize = parseInt(tankSize);
 function gasPriceCollectConvert(itemPrice) {
-    var itemCost = parseFloat(itemPrice);
-    itemCost = itemCost.toFixed(2);
-    itemCost = parseFloat(itemCost);
-    return itemCost;
+    if (typeof itemPrice == "string" && parseFloat(itemPrice) != "NaN") {
+        var itemCost = parseFloat(itemPrice);
+        itemCost = itemCost.toFixed(2);
+        itemCost = parseFloat(itemCost);
+        return itemCost;
+    }
+
 }
 costcoGas = gasPriceCollectConvert(costcoGas);
 walmartGas = gasPriceCollectConvert(walmartGas);
@@ -23,8 +26,11 @@ var totalCostco = totalCost(costcoGas, costcoMilk);
 var totalWalmart = totalCost(walmartGas, walmartMilk);
 document.open();
 
-if (totalCostco < totalWalmart) {
+if (typeof totalCostco != "number" || typeof totalWalmart != "number") {
+    document.write("<h1>Oh no</h1>")
+} else if (totalCostco < totalWalmart) {
     document.write("<h1>" + `You should go to Costco! With a gas price of $${costcoGas}, ${tankSize} gallons of gas and one gallon of milk will cost $${totalCostco} compared to Wal-Mart's $${totalWalmart}.` + "</h1>");
+;
 } else {
     document.write("<h1>" + `You should go to Wal-Mart. With a gas price of $${walmartGas}, ${tankSize} gallons of gas and one gallon of milk will cost $${totalWalmart} compared to Costco's $${totalCostco}.` + "</h1>");
 }
