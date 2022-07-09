ed8bc43c33b18f5f2f79c29fd785c2f4f51813ea
Jexzas <jdrmail2007@yahoo.com>
2022-07-08 22:26:28 -0500

    week 15 update

--- Week 8/script.js
+++
@@ -1,4 +0,0 @@
-function hiddenFunction() {
-    document.getElementById('hidden').setAttribute('class', 'gotem');
-    document.getElementById('hidden').innerHTML = `<a href="https://www.youtube.com/watch?v=kVpv8-5XWOI">Trains!</a>`;
-};
